4a2e43a436134c54033262d8840ea566a454e1d1
t.oda <takahiro.tech.oda@gmail.com>
2017-05-15 13:32:49 +0900

    Add RepositoryServiceProvider

---
+++ app/Providers/RepositoriesServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoriesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            \App\Repositories\GithubRepositoryInterface::class,
+            \App\Repositories\GithubRepository::class
+        );
+
+        $this->app->bind(
+            \App\Repositories\UserRepositoryInterface::class,
+            \App\Repositories\UserRepository::class
+        );
+    }
+}

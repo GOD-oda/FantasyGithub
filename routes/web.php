a4fdbb238bdc2c296b40ff5015ae96b79f83da26
Takahiro_Oda <GOD-oda@users.noreply.github.com>
2017-05-15 13:41:58 +0900

    Setup github oauth (#2)
    * Add laravel/socialite package
    
    * Add TopController and routes
    
    * Update users table migration and delete password_resets table
    
    * Move User.php to DataAccess/Eloquent directory from app directory
    
    * Add RepositoryServiceProvider
    
    * Set up github oauth
    
    * cosme User.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'TopController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+// authentication
+Route::get('login/', 'Auth\LoginController@login');
+Route::get('login/callback', 'Auth\LoginController@callback');
+
+Route::get('home', function () {
+    dd(true);
 });

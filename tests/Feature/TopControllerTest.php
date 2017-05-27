48c22355377a6747c8358e6519823e01376f36c9
Takahiro_Oda <GOD-oda@users.noreply.github.com>
2017-05-27 13:59:03 +0900

    Add TopController test (#8)
    - remove unnecessary files

--- tests/Feature/TopControllerTest.php
+++ tests/Feature/TopControllerTest.php
@@ -7,17 +7,15 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class TopControllerTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testIndex()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/')->assertStatus(200);
     }
 }
